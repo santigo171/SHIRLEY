8fba07a5fd7b9f0e06ef82af1290008025414c26
santigo171 <santibetancourt17@gmail.com>
2023-05-31 17:57:08 -0500

    EPIC COMMIT

--- SHIRLEY_ARDUINO/SHIRLEY_ARDUINO.ino
+++ SHIRLEY_ARDUINO/SHIRLEY_ARDUINO.ino
@@ -1,5 +1,7 @@
+// https://forum.arduino.cc/t/sd-card-connection-with-arduino-mega-2560/178436
+
 #include "SD.h"
-#define SD_ChipSelectPin 10
+#define SD_ChipSelectPin 53
 #include "TMRpcm.h"
 #include "SPI.h"
 
@@ -9,14 +11,22 @@ void setup()
 {
 tmrpcm.speakerPin=9;
 Serial.begin(9600);
+Serial.println("SHIRLEY Started");
+Serial.println(SD.begin(SD_ChipSelectPin));
 if(!SD.begin(SD_ChipSelectPin))
 {
   Serial.println("SD fail");
   return;
 }
 tmrpcm.setVolume(6);
-tmrpcm.play("test.wav");
 
+tmrpcm.play("C.wav");
+delay(3000);
+tmrpcm.play("D.wav");
+delay(3000);
+tmrpcm.play("E.wav");
+delay(3000);
+tmrpcm.play("A#.wav");
 }
 
 void loop() {
