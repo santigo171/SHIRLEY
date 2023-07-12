9c02b2525b1e18c89676bdaf822cc182afe41bbb
santigo171 <santibetancourt17@gmail.com>
2023-07-12 08:58:16 -0500

    Renamed and removed

--- SHIRLEY_ARDUINO/SHIRLEY_ARDUINO.ino
+++ SHIRLEY_ARDUINO/SHIRLEY_ARDUINO.ino
@@ -1,35 +1,37 @@
 // https://forum.arduino.cc/t/sd-card-connection-with-arduino-mega-2560/178436
 
 #include "SD.h"
-#define SD_ChipSelectPin 53
 #include "TMRpcm.h"
 #include "SPI.h"
+#include "pinsConnection.h"
 
 TMRpcm tmrpcm;
 
 void setup()
 {
-tmrpcm.speakerPin=9;
-Serial.begin(9600);
-Serial.println("SHIRLEY Started");
-Serial.println(SD.begin(SD_ChipSelectPin));
-if(!SD.begin(SD_ChipSelectPin))
-{
-  Serial.println("SD fail");
-  return;
-}
-tmrpcm.setVolume(6);
+  tmrpcm.speakerPin=9;
+  Serial.begin(9600);
+  Serial.println("SHIRLEY Started");
+  Serial.println(SD.begin(SD_CHIP_PIN));
+  if(!SD.begin(SD_CHIP_PIN))
+  {
+    Serial.println("SD fail");
+    return;
+  }
+  tmrpcm.setVolume(6);
 
-tmrpcm.play("C.wav");
-delay(3000);
-tmrpcm.play("D.wav");
-delay(3000);
-tmrpcm.play("E.wav");
-delay(3000);
-tmrpcm.play("A#.wav");
+  tmrpcm.play("C2.wav");
+  delay(3000);
+  
+  pinMode(C_BUTTON_PIN, INPUT);
 }
 
+int buttonState = 0; 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  buttonState = digitalRead(C_BUTTON_PIN);
+  if (buttonState == HIGH) {
+    Serial.println("BOTON");
+  } else {
+    Serial.println("NADA");
+  }
 }
---
+++ SHIRLEY_ARDUINO/pinsConnection.h
@@ -0,0 +1,19 @@
+#ifncddef PINSCONNECTION_H
+#define PINSCONNECTION_H
+
+#define SD_CHIP_PIN 53
+
+#define C_BUTTON_PIN 22
+#define C#_BUTTON_PIN 23
+#define D_BUTTON_PIN 24
+#define D#_BUTTON_PIN 25
+#define E_BUTTON_PIN 26
+#define F_BUTTON_PIN 27
+#define F#_BUTTON_PIN 28
+#define G_BUTTON_PIN 29
+#define G#_BUTTON_PIN 30
+#define A_BUTTON_PIN 31
+#define A#_BUTTON_PIN 32
+#define C2_BUTTON_PIN 33
+
+#endif
