fd180308a365dd01f8f336c3da518d2390f0049f
Estudiante <Estudiante@agustiniano.local>
2023-07-19 09:13:54 -0500

    Se crearon canciones perronas
    Para perro peroo

---
+++ assets/songs/Song6.ts
@@ -0,0 +1,189 @@
+import {
+    Whole,
+    Half,
+    Quarter,
+    QuarterD,
+    Eighth,
+    EighthD,
+    Semiquaver,
+    C,
+    Cs,
+    D,
+    Ds,
+    E,
+    F,
+    Fs,
+    G,
+    Gs,
+    A,
+    As,
+    B,
+    C2
+} from "./SongInterface";
+import { Song } from "./SongInterface";
+
+export const Alegria: Song = {
+    name: "Alegria 🌠",
+    bpm: 96,
+    metric: "4/4",
+    sheet: [
+        {
+            silence: false,
+            duration: Quarter,
+            note: [E]
+        },
+        {
+            silence: false,
+            duration: Quarter,
+            note: [E]
+        },
+        {
+            silence: false,
+            duration: Quarter,
+            note: [F]
+        },
+        {
+            silence: false,
+            duration: Quarter,
+            note: [G]
+        },
+        //
+        {
+            silence: false,
+            duration: Quarter,
+            note: [G]
+        },
+        {
+            silence: false,
+            duration: Quarter,
+            note: [F]
+        },
+        {
+            silence: false,
+            duration: Quarter,
+            note: [E]
+        },
+        {
+            silence: false,
+            duration: Quarter,
+            note: [D]
+        },
+        //
+        {
+            silence: false,
+            duration: Quarter,
+            note: [C]
+        },
+        {
+            silence: false,
+            duration: Quarter,
+            note: [C]
+        },
+        {
+            silence: false,
+            duration: Quarter,
+            note: [D]
+        },
+        {
+            silence: false,
+            duration: Quarter,
+            note: [E]
+        },
+        //
+        {
+            silence: false,
+            duration: QuarterD,
+            note: [E]
+        },
+        {
+            silence: false,
+            duration: Eighth,
+            note: [D]
+        },
+        {
+            silence: false,
+            duration: Half,
+            note: [D]
+        },
+        //
+        {
+            silence: false,
+            duration: Quarter,
+            note: [E]
+        },
+        {
+            silence: false,
+            duration: Quarter,
+            note: [E]
+        },
+        {
+            silence: false,
+            duration: Quarter,
+            note: [F]
+        },
+        {
+            silence: false,
+            duration: Quarter,
+            note: [G]
+        },
+        //
+        {
+            silence: false,
+            duration: Quarter,
+            note: [G]
+        },
+        {
+            silence: false,
+            duration: Quarter,
+            note: [F]
+        },
+        {
+            silence: false,
+            duration: Quarter,
+            note: [E]
+        },
+        {
+            silence: false,
+            duration: Quarter,
+            note: [D]
+        },
+        //
+        {
+            silence: false,
+            duration: Quarter,
+            note: [C]
+        },
+        {
+            silence: false,
+            duration: Quarter,
+            note: [C]
+        },
+        {
+            silence: false,
+            duration: Quarter,
+            note: [D]
+        },
+        {
+            silence: false,
+            duration: Quarter,
+            note: [E]
+        },
+        //
+        {
+            silence: false,
+            duration: QuarterD,
+            note: [E]
+        },
+        {
+            silence: false,
+            duration: Eighth,
+            note: [C]
+        },
+        {
+            silence: false,
+            duration: Half,
+            note: []
+        },
+        //
+    ]
+}
