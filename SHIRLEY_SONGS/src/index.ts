09d051901a3ed490ba0d794f0970919157ecfc5a
santigo171 <santibetancourt17@gmail.com>
2023-07-26 08:33:52 -0500

    Added code to export songs

--- SHIRLEY_SONGS/src/index.ts
+++ SHIRLEY_SONGS/src/index.ts
@@ -1,14 +1,32 @@
 import {Estrellita} from "../assets/songs/Song1"
-const songsToExport = [Estrellita];
+import {Cumpleannos} from "../assets/songs/Song2"
+import {Pirates} from "../assets/songs/Song3"
+import {Shark} from "../assets/songs/Song4"
+import {Sovietico} from "../assets/songs/Song5"
+import {Alegria} from "../assets/songs/Song6"
+import {Titanic} from "../assets/songs/Song7"
+import {Take_on_me} from "../assets/songs/Song8"
+
+import {silence} from "../assets/songs/SongInterface"
+
+import { writeFileSync } from 'fs'
+
+const songsToExport = [Estrellita, Cumpleannos, Pirates,
+    Shark,
+    Sovietico,
+    Alegria,
+    Titanic,
+    Take_on_me];
 const jsonToExport: any = {}
 
+let i = 0
 songsToExport.forEach((song) => {
     const songInJson = {
         name: song.name,
         bpm: song.bpm,
         notes: song.sheet.map((musicalFigure) => {
             if (musicalFigure.silence === true) {
-                return 13
+                return silence
             } else {
                 return musicalFigure.note
             }
@@ -17,8 +35,16 @@ songsToExport.forEach((song) => {
             return musicalFigure.duration
         })
     }
-    jsonToExport[song.name] = songInJson
+    jsonToExport[i] = songInJson
+    i++
     console.log(`written ${song.name}`)
 })
+
+try {
+    writeFileSync('./build/songs.json', JSON.stringify(jsonToExport))
+    console.log("File written")
+} catch (err) {
+    console.error(err)
+}
 
 console.log(jsonToExport)
