1f22e923bbb67a030f69783e739b48f34e220175
santigo171 <santibetancourt17@gmail.com>
2023-08-02 08:29:33 -0500

    Modified arduino

--- SHIRLEY_ARDUINO/SHIRLEY_ARDUINO.ino
+++ SHIRLEY_ARDUINO/SHIRLEY_ARDUINO.ino
@@ -4,6 +4,7 @@
 #include "TMRpcm.h"
 #include "SPI.h"
 #include "pinsConnection.h"
+#include "ArduinoJson.h"
 
 TMRpcm tmrpcm;
 
@@ -11,7 +12,7 @@ void setup()
 {
   tmrpcm.speakerPin=9;
   Serial.begin(9600);
-  Serial.println("SHIRLEY Started");
+  Serial.println("\nSHIRLEY Started");
   Serial.println(SD.begin(SD_CHIP_PIN));
   if(!SD.begin(SD_CHIP_PIN))
   {
@@ -24,14 +25,36 @@ void setup()
   delay(3000);
   
   pinMode(C_BUTTON_PIN, INPUT);
+  pinMode(CS_BUTTON_PIN, INPUT);
+  pinMode(D_BUTTON_PIN, INPUT);
+  pinMode(DS_BUTTON_PIN, INPUT);
+  pinMode(E_BUTTON_PIN, INPUT);
+  pinMode(F_BUTTON_PIN, INPUT);
+  pinMode(FS_BUTTON_PIN, INPUT);
+  pinMode(G_BUTTON_PIN, INPUT);
+  pinMode(GS_BUTTON_PIN, INPUT);
+  pinMode(A_BUTTON_PIN, INPUT);
+  pinMode(AS_BUTTON_PIN, INPUT);
+  pinMode(C2_BUTTON_PIN, INPUT);
+
+  File songs = SD.open("songs.json");
+  if (songs) {
+    Serial.println("songs.txt:");
+    while (songs.available()) {
+      Serial.write(songs.read());
+    }
+    songs.close();
+  } else {
+    Serial.println("error opening file");
+  }
 }
 
 int buttonState = 0; 
 void loop() {
   buttonState = digitalRead(C_BUTTON_PIN);
   if (buttonState == HIGH) {
-    Serial.println("BOTON");
+    // Serial.println("BOTON");
   } else {
-    Serial.println("NADA");
+    // Serial.println("NADA");
   }
 }
